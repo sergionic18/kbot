5e478c1031ccb858dec6f30351b8dba2d6f1883d
sergionic18 <sergionic89@gmail.com>
2023-12-19 16:02:15 +0200

    current status with version and kbot updated

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -14,11 +11,9 @@ import (
 )
 
 var (
-	// Teletoken bot
 	Teletoken = os.Getenv("TELE_TOKEN")
 )
 
-// bottCmd represents the bott command
 var bottCmd = &cobra.Command{
 	Use:     "kbot",
 	Aliases: []string{"start"},
@@ -61,13 +56,4 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(bottCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// bottCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// bottCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,9 +6,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var appVersion = "Vers"
+var appVersion = "Ver"
 
-// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "A brief description of your command",
@@ -22,20 +18,11 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(appVersion)
+		fmt.Printf("version: %s\n", appVersion)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
